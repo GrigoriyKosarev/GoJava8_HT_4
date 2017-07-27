2f4c47b10e2eda7af1fdef6b79248aa62046b66e
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2017-07-27 12:36:50 +0300

    Задание 1. Готово Вывести числа от 0 к максимум

--- src/Functions.java
+++ src/Functions.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 /**
  * Created by admin on 27.07.2017.
  */
 public class Functions {
+
     public static void main(String[] args) {
-        
+
+        //---Задание 1-----------------------------
+        System.out.println(countToMAXNumber(5, 1));
+
+    }
+
+    public static int countToMAXNumber(int maxInt, int i){
+
+        if (maxInt == i){
+            return i;
+        }
+        else{
+            System.out.println(i);
+            countToMAXNumber(5, i+1);
+        }
+
+        return maxInt;
     }
 }
