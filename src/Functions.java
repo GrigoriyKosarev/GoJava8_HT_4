168c8526c149ad2b0d008102b8c6d1eed23bc0a0
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2017-07-27 16:22:18 +0300

    Задание 2. Готово

--- src/Functions.java
+++ src/Functions.java
@@ -1,37 +1,47 @@
 import java.util.Scanner;
 
-/**
- * Created by admin on 27.07.2017.
- */
 public class Functions {
 
     public static void main(String[] args) {
 
-        //---Задание 1-----------------------------
+        System.out.println("---Задание 1-----------------------------");
         int x = 5;
         countToMAXNumber(x);
 
-        /*---Задание 2-------------------------------------
-        Написать функцию drawRectangle которая рисует в консоли прямоугольник из символов '+'
+        System.out.println("---Задание 2-----------------------------");
+        /*Написать функцию drawRectangle которая рисует в консоли прямоугольник из символов '+'
         Аргументы функции: ширина прямугольника в символах, высота прямоугольника в символах*/
-
+        drawRectangle("3", "2");
 
     }
 
-    public static String countToMAXNumber(int maxInt){
+    public static void countToMAXNumber(int maxInt){
 
         // Базовый случай
         if (maxInt == 1) {
             System.out.println(maxInt);
-            return "1";
+            return;
         }
         // Шаг рекурсии / рекурсивное условие
         countToMAXNumber(maxInt - 1);
 
         System.out.println(maxInt);
 
-        return "1";
+        return;
     }
 
+    public static void drawRectangle(String width, String heigh){
+
+        int widtheRectangle = Integer.parseInt(width);
+        int heighRectangle = Integer.parseInt(heigh);
+
+        for (int i = 0; i < heighRectangle; i++){
+            for (int j = 0; j < widtheRectangle; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
 
 }
